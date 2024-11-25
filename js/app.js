0581660dbe55c27f0fe0238349e04539d7a1e52e
obezeq <eortbra2812@g.educaand.es>
2024-11-25 21:34:13 +0100

    sign changes and home upgrades

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const toggleTheme = () => {
+    const body = document.body;
+    const currentTheme = body.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    body.setAttribute("data-theme", newTheme);
+}
+
+const words = ["programmers", "tech lovers", "gamers", "designers"];
+
+let i = 0;
+let timer;
+
+function typeWriter() {
+    let word = words[i].split("");
+    let loopTyping = function() {
+        if (word.length > 0) {
+            document.querySelector(".typing").innerHTML += word.shift();
+        } else {
+            deleteWord();
+            return false;
+        }
+        timer = setTimeout(loopTyping, 150);
+    };
+    loopTyping();
+}
+
+function deleteWord() {
+    let word = words[i].split("");
+    let loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.querySelector(".typing").innerHTML = word.join("");
+        } else {
+            if (i < words.length - 1) {
+                i++;
+            } else {
+                i = 0;
+            }
+            typeWriter();
+            return false;
+        }
+        timer = setTimeout(loopDeleting, 100);
+    };
+    setTimeout(loopDeleting, 1500);
+}
+
+function parallaxEffect(){
+    document.addEventListener('scroll', function() {
+        const descriptions = document.querySelector('.home__descriptions');
+        const windowHeight = window.innerHeight;
+        const elementTop = descriptions.getBoundingClientRect().top;
+    
+        // Check if the element is in the viewport
+        if (elementTop < windowHeight) {
+            descriptions.classList.add('visible');
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    typeWriter();
+    parallaxEffect();
+});
